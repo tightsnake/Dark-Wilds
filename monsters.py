02dd816f69ec2c1a81dca13ebdbe71e045f35db7
Chris Lefkarites <clefkari@ucsd.edu>
2022-07-31 21:03:40 -0700

    Iteration 2

--- monsters.py
+++ monsters.py
@@ -1,10 +1,11 @@
+from system import clear_screen
 from ascii_art import art_dict
 from stats import BaseStats, CurrentStats
 
 class Monster(BaseStats, CurrentStats):
-    def __init__(self, name, strength, agility, intelligence, constitution, mana):
-        BaseStats.__init__(self, strength, agility, intelligence, constitution, mana)
-        CurrentStats.__init__(self, strength, agility, intelligence, constitution, mana)
+    def __init__(self, name, strength, agility, intelligence, constitution):
+        BaseStats.__init__(self, strength, agility, intelligence, constitution)
+        CurrentStats.__init__(self, strength, agility, intelligence, constitution)
         self.name = name
         self.art = art_dict[name]
 
@@ -13,14 +14,19 @@ class Monster(BaseStats, CurrentStats):
         print(f"Agility: {self.agility}")
         print(f"Intelligence: {self.intelligence}")
         print(f"Constitution: {self.constitution}")
-        print(f"Mana: {self.mana}")
 
     def print(self):
         print(f"{self.name}\n")
         self.print_current_stats()
         print(self.art)
 
+ghost = Monster("Ghost", 1,1,1,1)
+devil = Monster("Devil", 1,1,1,1)
 
-
-ghost = Monster("Ghost", 1,1,1,1,1)
-ghost.print()
+while(True):
+    input()
+    clear_screen()
+    ghost.print()
+    input()
+    clear_screen()
+    devil.print()
--- stats.py
+++ stats.py
@@ -1,15 +1,15 @@
 class BaseStats:
-    def __init__(self, strength, agility, intelligence, constitution, mana):
+    def __init__(self, strength, agility, intelligence, constitution):
         self.strength = strength
         self.agility = agility
         self.intelligence = intelligence
         self.constitution = constitution
-        self.mana = mana
+        
 
 class CurrentStats:
-    def __init__(self, strength, agility, intelligence, constitution, mana):
+    def __init__(self, strength, agility, intelligence, constitution):
         self.strength = strength
         self.agility = agility
         self.intelligence = intelligence
         self.constitution = constitution
-        self.mana = mana
+        
